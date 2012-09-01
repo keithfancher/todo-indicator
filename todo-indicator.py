95d1e40920ff7f11dd68dc34ef2bb155e2881c62
ktf <not.discostoo@gmail.com>
2012-08-31 17:21:32 -0700

    read in todo.txt file, add edit menu item

--- todo-indicator.py
+++ todo-indicator.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
 
+import os
+
 from gi.repository import Gtk
 from gi.repository import AppIndicator3 as appindicator
 
 
 DARK_PANEL_ICON = "gtg-panel"
-TESTING_LIST = ["(A) item 1", "(A) item 2", "(B) item 3", "(C) another item"]
+TESTING_TODO_FILE = "todo.txt"
+EDITOR = "gvim"
+
+
+def edit_handler(event):
+    os.system(EDITOR + " " + TESTING_TODO_FILE)
 
 
 def quit_handler(event):
@@ -14,6 +21,13 @@ def quit_handler(event):
 
 
 def main():
+    # open the file, read it
+    todo_file = open(TESTING_TODO_FILE)
+    todo_list = todo_file.read().split("\n")
+    del todo_list[-1] # kill final empty entry
+    todo_file.close()
+
+    # indicator business
     ind = appindicator.Indicator.new("todo-txt-indicator", DARK_PANEL_ICON,
                                      appindicator.IndicatorCategory.OTHER)
     ind.set_status(appindicator.IndicatorStatus.ACTIVE)
@@ -23,13 +37,19 @@ def main():
     menu = Gtk.Menu()
 
     # create todo menu items
-    for todo_item in TESTING_LIST:
+    for todo_item in todo_list:
         menu_item = Gtk.MenuItem(todo_item)
         menu_item.show()
         menu.append(menu_item)
 
     # add a separator
     menu_item = Gtk.SeparatorMenuItem()
+    menu_item.show()
+    menu.append(menu_item)
+
+    # add "edit list" menu item
+    menu_item = Gtk.MenuItem("Edit")
+    menu_item.connect("activate", edit_handler)
     menu_item.show()
     menu.append(menu_item)
 
