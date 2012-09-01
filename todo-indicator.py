fc7a1ca94c1543d19599ade84883836683f5d5ac
ktf <not.discostoo@gmail.com>
2012-08-31 18:18:45 -0700

    make the indicator an object, add refresh functionality

--- todo-indicator.py
+++ todo-indicator.py
@@ -12,69 +12,81 @@ TESTING_TODO_FILE = "todo.txt"
 EDITOR = "gvim"
 
 
-def read_todo_file(filename):
-    """Returns a list of todo items read from the given file name."""
-    todo_file = open(filename)
-    todo_list = todo_file.read().split("\n")
-    todo_file.close()
-    todo_list = filter(None, todo_list) # kill empty items
-    return sorted(todo_list)
+class TodoIndicator(object):
 
+    def __init__(self):
+        self.todo_list = self._read_todo_file(TESTING_TODO_FILE)
+        self.ind = self._build_indicator(self.todo_list)
 
-def edit_handler(event):
-    """Opens the todo.txt file with selected editor."""
-    os.system(EDITOR + " " + TESTING_TODO_FILE)
+    def _read_todo_file(self, filename):
+        """Returns a list of todo items read from the given file name."""
+        todo_file = open(filename)
+        todo_list = todo_file.read().split("\n")
+        todo_file.close()
+        todo_list = filter(None, todo_list) # kill empty items
+        return sorted(todo_list)
 
+    def _edit_handler(self, event):
+        """Opens the todo.txt file with selected editor."""
+        os.system(EDITOR + " " + TESTING_TODO_FILE)
 
-def quit_handler(event):
-    """Quits our fancy little program."""
-    Gtk.main_quit()
+    def _refresh_handler(self, event):
+        """Refreshes the list."""
+        # TODO: gives odd warning about removing a child...
+        self.todo_list = self._read_todo_file(TESTING_TODO_FILE)
+        self.ind = self._build_indicator(self.todo_list)
 
+    def _quit_handler(self, event):
+        """Quits our fancy little program."""
+        Gtk.main_quit()
 
-def build_indicator(todo_list):
-    """Builds the Indicator object."""
-    ind = appindicator.Indicator.new("todo-txt-indicator", DARK_PANEL_ICON,
-                                     appindicator.IndicatorCategory.OTHER)
-    ind.set_status(appindicator.IndicatorStatus.ACTIVE)
+    def _build_indicator(self, todo_list):
+        """Builds the Indicator object."""
+        ind = appindicator.Indicator.new("todo-txt-indicator", DARK_PANEL_ICON,
+                                         appindicator.IndicatorCategory.OTHER)
+        ind.set_status(appindicator.IndicatorStatus.ACTIVE)
 
-    # create a menu
-    menu = Gtk.Menu()
+        # create a menu
+        menu = Gtk.Menu()
 
-    # create todo menu items
-    for todo_item in todo_list:
-        menu_item = Gtk.MenuItem(todo_item)
+        # create todo menu items
+        for todo_item in todo_list:
+            menu_item = Gtk.MenuItem(todo_item)
+            menu_item.show()
+            menu.append(menu_item)
+
+        # add a separator
+        menu_item = Gtk.SeparatorMenuItem()
         menu_item.show()
         menu.append(menu_item)
 
-    # add a separator
-    menu_item = Gtk.SeparatorMenuItem()
-    menu_item.show()
-    menu.append(menu_item)
+        # add "edit list" menu item
+        menu_item = Gtk.MenuItem("Edit todo.txt")
+        menu_item.connect("activate", self._edit_handler)
+        menu_item.show()
+        menu.append(menu_item)
 
-    # add "edit list" menu item
-    menu_item = Gtk.MenuItem("Edit todo.txt")
-    menu_item.connect("activate", edit_handler)
-    menu_item.show()
-    menu.append(menu_item)
+        # add "refresh" menu item
+        menu_item = Gtk.MenuItem("Refresh")
+        menu_item.connect("activate", self._refresh_handler)
+        menu_item.show()
+        menu.append(menu_item)
 
-    # add quit menu item
-    menu_item = Gtk.MenuItem("Quit")
-    menu_item.connect("activate", quit_handler)
-    menu_item.show()
-    menu.append(menu_item)
+        # add quit menu item
+        menu_item = Gtk.MenuItem("Quit")
+        menu_item.connect("activate", self._quit_handler)
+        menu_item.show()
+        menu.append(menu_item)
 
-    ind.set_menu(menu)
+        ind.set_menu(menu)
 
-    return ind
+        return ind
 
-
-def main():
-    """Our main man!"""
-    todo_list = read_todo_file(TESTING_TODO_FILE)
-
-    ind = build_indicator(todo_list)
-    Gtk.main()
+    def main(self):
+        """The indicator's main loop."""
+        Gtk.main()
 
 
 if __name__ == "__main__":
-    main()
+    ind = TodoIndicator()
+    ind.main()
