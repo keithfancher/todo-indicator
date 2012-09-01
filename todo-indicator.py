03a51055250a17d8480603ba2e86022c50aee126
ktf <not.discostoo@gmail.com>
2012-08-31 17:01:01 -0700

    show the list, sort of, and a quit button

--- todo-indicator.py
+++ todo-indicator.py
@@ -1,42 +1,43 @@
 #!/usr/bin/env python
 
 
-import sys
-
 from gi.repository import Gtk
 from gi.repository import AppIndicator3 as appindicator
 
 
 DARK_PANEL_ICON = "gtg-panel"
+TESTING_LIST = ["(A) item 1", "(A) item 2", "(B) item 3", "(C) another item"]
 
 
-def menuitem_response(w, buf):
-    print buf
-    sys.exit(0)
+def quit_handler(event):
+    Gtk.main_quit()
 
 
 def main():
-    ind = appindicator.Indicator.new(
-        "todo-txt-indicator",
-        DARK_PANEL_ICON,
-        appindicator.IndicatorCategory.OTHER)
+    ind = appindicator.Indicator.new("todo-txt-indicator", DARK_PANEL_ICON,
+                                     appindicator.IndicatorCategory.OTHER)
     ind.set_status(appindicator.IndicatorStatus.ACTIVE)
 #    ind.set_attention_icon("indicator-messages-new") # necessary?
 
     # create a menu
     menu = Gtk.Menu()
 
-    # create some
-    for i in range(3):
-        buf = "Test-undermenu - %d" % i
-        menu_items = Gtk.MenuItem(buf)
-        menu.append(menu_items)
+    # create todo menu items
+    for todo_item in TESTING_LIST:
+        menu_item = Gtk.MenuItem(todo_item)
+        menu_item.show()
+        menu.append(menu_item)
 
-        # this is where you would connect your menu item up with a function:
-        menu_items.connect("activate", menuitem_response, buf)
+    # add a separator
+    menu_item = Gtk.SeparatorMenuItem()
+    menu_item.show()
+    menu.append(menu_item)
 
-        # show the items
-        menu_items.show()
+    # add quit menu item
+    menu_item = Gtk.MenuItem("Quit")
+    menu_item.connect("activate", quit_handler)
+    menu_item.show()
+    menu.append(menu_item)
 
     ind.set_menu(menu)
     Gtk.main()
