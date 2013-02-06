d9ff2f3194119f4ea1684fbf9724358e66976c62
ktf <not.discostoo@gmail.com>
2013-02-05 20:57:41 -0800

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 
 setup(name='todo_indicator',
-      version='0.1.0-dev',
+      version='0.2.0',
       description='Ubuntu app indicator for todo.txt-style todo lists',
       author='Keith Fancher',
       author_email='keith.fancher@gmail.com',
