67572da2c8ce2f358be18a207fea633090df9603
ktf <not.discostoo@gmail.com>
2014-03-15 17:13:10 -0700

    add tests for item initialization

---
+++ test_todo_indicator.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+
+import unittest
+
+from todo_indicator import TodoTxtItem
+
+
+class TestTodoTxtItem(unittest.TestCase):
+
+    def test_init_from_text_completion(self):
+        completed_item = 'x This is a completed item'
+        completed_whitespace = '    x Also completed    '
+        not_completed = 'xNope, not even close'
+        also_not_completed = '   Nope, not even close'
+
+        test_item = TodoTxtItem()
+        test_item.init_from_text(completed_item)
+        self.assertTrue(test_item.is_completed)
+        self.assertEqual(test_item.text, 'This is a completed item')
+
+        test_item.init_from_text(completed_whitespace)
+        self.assertTrue(test_item.is_completed)
+        self.assertEqual(test_item.text, 'Also completed')
+
+        test_item.init_from_text(not_completed)
+        self.assertFalse(test_item.is_completed)
+        self.assertEqual(test_item.text, 'xNope, not even close')
+
+        test_item.init_from_text(also_not_completed)
+        self.assertFalse(test_item.is_completed)
+        self.assertEqual(test_item.text, 'Nope, not even close')
+
+    def test_init_from_text_priority(self):
+        item_with_priority = '(A) This item is pretty important'
+        item_with_priority2 = '(Z) Not so important'
+        no_priority = 'Just some item'
+        no_priority2 = '(b) Only caps should work here'
+
+        test_item = TodoTxtItem()
+        test_item.init_from_text(item_with_priority)
+        self.assertEqual(test_item.priority, 'A')
+        self.assertEqual(test_item.text, 'This item is pretty important')
+
+        test_item.init_from_text(item_with_priority2)
+        self.assertEqual(test_item.priority, 'Z')
+        self.assertEqual(test_item.text, 'Not so important')
+
+        test_item.init_from_text(no_priority)
+        self.assertEqual(test_item.priority, None)
+        self.assertEqual(test_item.text, 'Just some item')
+
+        test_item.init_from_text(no_priority2)
+        self.assertEqual(test_item.priority, None)
+        self.assertEqual(test_item.text, '(b) Only caps should work here')
+
+    def test_init_from_text_completion_and_priority(self):
+        item = 'x (R) Good thing I completed this'
+
+        test_item = TodoTxtItem()
+        test_item.init_from_text(item)
+
+        self.assertTrue(test_item.is_completed)
+        self.assertEqual(test_item.priority, 'R')
+        self.assertEqual(test_item.text, 'Good thing I completed this')
+
+
+if __name__ == '__main__':
+    unittest.main()
