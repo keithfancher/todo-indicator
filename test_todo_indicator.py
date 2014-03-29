f63b155514b703c9ed3464b79d7518c71cd413b9
ktf <not.discostoo@gmail.com>
2014-03-29 15:17:57 -0700

    add test for loading list from file

--- test_todo_indicator.py
+++ test_todo_indicator.py
@@ -104,6 +104,44 @@ class TestTodoTxtList(unittest.TestCase):
         self.assertEqual(None, test_list.items[2].priority)
         self.assertTrue(test_list.items[2].is_completed)
 
+    def test_init_from_file(self):
+        file_name = 'sample-todo.txt'
+        test_list = TodoTxtList(file_name)
+
+        self.assertEqual(8, test_list.num_items())
+
+        self.assertEqual('Do that really important thing', test_list.items[0].text)
+        self.assertEqual('A', test_list.items[0].priority)
+        self.assertFalse(test_list.items[0].is_completed)
+
+        self.assertEqual('Summon AppIndicator documentation from my ass', test_list.items[1].text)
+        self.assertEqual('D', test_list.items[1].priority)
+        self.assertFalse(test_list.items[1].is_completed)
+
+        self.assertEqual('This other important thing', test_list.items[2].text)
+        self.assertEqual('A', test_list.items[2].priority)
+        self.assertFalse(test_list.items[2].is_completed)
+
+        self.assertEqual('Walk the cat', test_list.items[3].text)
+        self.assertEqual('B', test_list.items[3].priority)
+        self.assertFalse(test_list.items[3].is_completed)
+
+        self.assertEqual('Something with no priority!', test_list.items[4].text)
+        self.assertEqual(None, test_list.items[4].priority)
+        self.assertFalse(test_list.items[4].is_completed)
+
+        self.assertEqual('Cook the dog', test_list.items[5].text)
+        self.assertEqual('C', test_list.items[5].priority)
+        self.assertFalse(test_list.items[5].is_completed)
+
+        self.assertEqual('Be annoyed at GTK3 docs', test_list.items[6].text)
+        self.assertEqual(None, test_list.items[6].priority)
+        self.assertTrue(test_list.items[6].is_completed)
+
+        self.assertEqual('Something I already did', test_list.items[7].text)
+        self.assertEqual(None, test_list.items[7].priority)
+        self.assertTrue(test_list.items[7].is_completed)
+
 
 if __name__ == '__main__':
     unittest.main()
